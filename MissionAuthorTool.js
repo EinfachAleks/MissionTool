74c6495ce0687fa92677a8f5eb9313530291ee9b
EinfachAleks <aleks@einfach-aleks.com>
2019-05-22 16:52:50 +0200

    Update
    - create Version 0.4

--- MissionAuthorTool.js
+++ MissionAuthorTool.js
@@ -1,93 +1,42 @@
 // ==UserScript==
 // @name                Mission View Changer
-// @version             0.3
+// @version             0.4
 // @description         Restyle the View of Mission Creator Tool and add new Features
 // @author              EinfachAleks, https://t.me/EinfachAleks
 // @license             MIT
 // @homepageURL         https://github.com/EinfachAleks/MissionTool
-// @contributionAmoun   https://github.com/EinfachAleks/MissionTool
+// @contributionURL     https://github.com/EinfachAleks/MissionTool
 // @match               https://mission-author-dot-betaspike.appspot.com/
 // @icon                https://raw.githubusercontent.com/EinfachAleks/MissionTool/master/image/specops.ico
-// @grant               unsafeWindow
-// @grant               GM_addStyle
-// @grant               GM_getResourceText
 // @downloadURL         https://raw.githubusercontent.com/EinfachAleks/MissionTool/master/MissionAuthorTool.js
 // @updateURL           https://raw.githubusercontent.com/EinfachAleks/MissionTool/master/MissionAuthorTool.js
 // @issues              https://github.com/EinfachAleks/MissionTool/issues
 // @require             https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js
 // @require             https://cdnjs.cloudflare.com/ajax/libs/tinysort/3.2.5/tinysort.min.js
 // @resource            mainCss https://raw.githubusercontent.com/EinfachAleks/MissionTool/master/style.css
+// @grant               unsafeWindow
+// @grant               GM_addStyle
+// @grant               GM_getResourceText
 // ==/UserScript==
 
 (function ($) {
     "use strict";
 
     // Global Variables
-    const regEx = /\bM{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\b/gsm;
-    const $namePublished = $('span.mission-title-published');
-    const $nameSubmitted = $('span.mission-title-submitted');
-    const $nameDrafts = $('span.mission-title-draft_of_published_mission');
-    const $nameSubmittedPublished = $('span.mission-title-submitted_and_published');
     const $list = $('.missions-list .mission');
 
-    // Call init function
+    /**
+     * @name Init
+     * @description call the Init Function
+     */
     $(function () {
         setTimeout(function () {
-            init();
+
+            editorButton();
+            missionStatusSetter();
+            missionSorter();
         }, 1000);
     });
-
-    function init() {
-        nameFinder();
-        editorButton();
-        missionStatusSetter();
-        missionSorter();
-    }
-
-    // find Name of listed Missions
-    function nameFinder() {
-        let $splitName = [];
-        let $nameArray = [].concat(...$namePublished, ...$nameSubmitted, ...$nameSubmittedPublished, ...$nameDrafts);
-        $.each($nameArray, function (index, value) {
-            let name = $(value).text().trim();
-            console.log("name", name);
-            $splitName = [].concat(...$splitName, name);
-        });
-        $splitName = [].concat(...$splitName);
-        $.each($splitName, function (index, value) {
-            // console.log("nameFinder", value);
-            romanToArabic(value);
-        });
-    }
-
-    // Roman Number Converter
-    function romanToArabic(value) {
-        let newValue = "";
-        let newNumber;
-        let $splittedArray = value.match(regEx);
-
-        function fromRoman(value) {
-            let result = 0;
-            let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-            let roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-            for (let i = 0; i <= decimal.length; i++) {
-                while (value.indexOf(roman[i]) === 0) {
-                    result += decimal[i];
-                    value = value.replace(roman[i], '');
-                }
-            }
-            return result;
-        }
-
-        $.each($splittedArray, function (index, value) {
-            if ("" !== value) {
-                newNumber = fromRoman(value);
-                newValue = value.replace(regEx, newNumber);
-                let newValueLength = newValue.toString().length;
-                newValue = newValue.slice(0,  newValueLength/ 2);
-            }
-        });
-    }
 
     /**
      * @name missionStatusSetter
@@ -184,5 +133,5 @@
 
 })(jQuery);
 
-// let mainCss = GM_getResourceText("mainCss");
-// GM_addStyle(mainCss);
+let mainCss = GM_getResourceText("mainCss");
+GM_addStyle(mainCss);
